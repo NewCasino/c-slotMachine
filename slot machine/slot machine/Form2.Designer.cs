332bac24be89511992e2283f2a26badaa6dbf77d
joshua1996 <kjw1996@hotmail.com>
2015-12-07 17:26:40 +0800

    load function

--- slot machine/slot machine/Form2.Designer.cs
+++ slot machine/slot machine/Form2.Designer.cs
@@ -34,15 +34,19 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(99, 161);
+            this.pictureBox1.Location = new System.Drawing.Point(45, 200);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(168, 177);
+            this.pictureBox1.Size = new System.Drawing.Size(168, 121);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -57,9 +61,9 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(76, 20);
+            this.pictureBox2.Location = new System.Drawing.Point(45, 98);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(312, 135);
+            this.pictureBox2.Size = new System.Drawing.Size(168, 76);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
@@ -78,11 +82,29 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(219, 98);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(168, 76);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(219, 200);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(168, 121);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(633, 600);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button1);
@@ -92,6 +114,8 @@
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -103,5 +127,7 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
--- slot machine/slot machine/Form2.cs
+++ slot machine/slot machine/Form2.cs
@@ -12,7 +12,9 @@ namespace slot_machine
 {
     public partial class Form2 : Form
     {
-        const int speed = 1;
+        const int speed = 20;
+        int up;
+        int down;
         public Form2()
         {
             InitializeComponent();
@@ -20,26 +22,61 @@ namespace slot_machine
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            pictureBox1.Width = Properties.Resources._851592_279586392215626_1440866487_n.Width;
-            pictureBox1.Height = Properties.Resources._851592_279586392215626_1440866487_n.Height;
+            pictureboxWidtHeight();
+            buttonLocation();
+            pictureboxLocation();
+            pictureboxImage();
+        }
+        
+        private void pictureboxImage()
+        {
+            pictureBox1.Image = Properties.Resources.reel_strip;
+            pictureBox2.Image = Properties.Resources.reel_strip;
+            pictureBox3.Image = Properties.Resources.reel_strip;
+            pictureBox4.Image = Properties.Resources.reel_strip;
+        }
 
-            pictureBox2.Width = Properties.Resources._851592_279586392215626_1440866487_n.Width;
-            pictureBox2.Height = Properties.Resources._851592_279586392215626_1440866487_n.Height;
+        private void pictureboxLocation()
+        {
+            pictureBox2.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - Properties.Resources.reel_strip.Height);
+            pictureBox3.Location = new Point(pictureBox4.Location.X, pictureBox4.Location.Y - Properties.Resources.reel_strip.Height);
+        }
 
+        private void buttonLocation()
+        {
             button1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y);
-            button2.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y+ Properties.Resources._851592_279586392215626_1440866487_n.Height);
-            pictureBox2.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - Properties.Resources._851592_279586392215626_1440866487_n.Height);
+            button2.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + Properties.Resources.reel_strip.Height);
+            up = button1.Location.Y;
+            down = button2.Location.Y;
+        }
 
-            pictureBox1.Image = Properties.Resources._851592_279586392215626_1440866487_n;
-            pictureBox2.Image = Properties.Resources._851592_279586392215626_1440866487_n;
+        private void pictureboxWidtHeight()
+        {
+            pictureBox1.Width = Properties.Resources.reel_strip.Width;
+            pictureBox1.Height = Properties.Resources.reel_strip.Height;
+
+            pictureBox2.Width = Properties.Resources.reel_strip.Width;
+            pictureBox2.Height = Properties.Resources.reel_strip.Height;
+
+            pictureBox3.Width = Properties.Resources.reel_strip.Width;
+            pictureBox3.Height = Properties.Resources.reel_strip.Height;
+
+            pictureBox4.Width = Properties.Resources.reel_strip.Width;
+            pictureBox4.Height = Properties.Resources.reel_strip.Height;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + speed);
             pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y + speed);
-
-           // if (pictureBox1.Location.Y - )
+            if (pictureBox1.Location.Y >= down)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, button1.Location.Y - Properties.Resources.reel_strip.Height);
+            }
+            if (pictureBox2.Location.Y >= down)
+            {
+                pictureBox2.Location = new Point(pictureBox1.Location.X, button1.Location.Y - Properties.Resources.reel_strip.Height);
+            }
         }
     }
 }
